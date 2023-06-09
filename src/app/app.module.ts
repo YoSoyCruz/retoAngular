b8e4b64ae37987024fdd41a88d36ad8ed8ce62a5
YoSoyCruz <c.leonardo.ortiz30@gmail.com>
2023-06-08 23:57:16 -0600

    Se instala Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoremModule } from './lorem/lorem.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { LoremModule } from './lorem/lorem.module';
   ],
   imports: [
     BrowserModule,
-    LoremModule
+    LoremModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
