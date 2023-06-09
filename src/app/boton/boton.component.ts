b61c950c100ad20c0316ac05b15b0083415a606d
YoSoyCruz <c.leonardo.ortiz30@gmail.com>
2023-06-08 23:42:18 -0600

    se crea modulo y compomente para el botón

---
+++ src/app/boton/boton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton',
+  templateUrl: './boton.component.html',
+  styles: [
+  ]
+})
+export class BotonComponent {
+
+}
---
+++ src/app/boton/boton.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BotonComponent } from './boton.component';
+
+
+
+@NgModule({
+  declarations: [
+    BotonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BotonModule { }
